adc8b144011c8fd080a4ba9c9b2dd73f694be455
1230505027 <153460547+ecemreer@users.noreply.github.com>
2025-02-15 13:04:12 +0300

    Update Güzergah.java

--- src/Güzergah.java
+++ src/Güzergah.java
@@ -1,16 +1,16 @@
 import java.util.List;
 
 public class Güzergah {
-    private List<Mahalle> mahalleler; // Seçilen mahallelerden oluşan güzergah
-    private double toplamFayda; // Güzergahın toplam fayda değeri
-
-    // **Kurucu Metot (Constructor)**
+    private List<Mahalle> mahalleler; 
+    private double toplamFayda;
+    
+   
     public Güzergah(List<Mahalle> mahalleler, double toplamFayda) {
         this.mahalleler = mahalleler;
         this.toplamFayda = toplamFayda;
     }
 
-    // **Güzergahı ekrana yazdıran metot**
+  
     public void yazdir() {
         System.out.println("Seçilen en uygun güzergah:");
         for (Mahalle mahalle : mahalleler) {
